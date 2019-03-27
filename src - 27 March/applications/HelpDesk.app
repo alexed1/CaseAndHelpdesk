e436c61a21674b563f2b74e0b8165ea03d1c3235
Shubham <shubham.dadhich@mtxb2b.com>
2019-03-27 18:11:17 +0530

    Updated 27 March 19

---
+++ src - 27 March/applications/HelpDesk.app
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<CustomApplication xmlns="http://soap.sforce.com/2006/04/metadata">
+    <brand>
+        <headerColor>#0070D2</headerColor>
+        <shouldOverrideOrgTheme>false</shouldOverrideOrgTheme>
+    </brand>
+    <description>Create and manage helpdesk ticket</description>
+    <formFactors>Large</formFactors>
+    <isNavAutoTempTabsDisabled>false</isNavAutoTempTabsDisabled>
+    <isNavPersonalizationDisabled>false</isNavPersonalizationDisabled>
+    <label>Help Desk</label>
+    <navType>Standard</navType>
+    <tabs>standard-home</tabs>
+    <tabs>HelpDesk_Ticket</tabs>
+    <uiType>Lightning</uiType>
+    <utilityBar>HelpDesk_UtilityBar</utilityBar>
+</CustomApplication>
---
+++ src - 27 March/aura/caseMgmt_CaseStatusDualListBox/caseMgmt_CaseStatusDualListBoxController.js
@@ -0,0 +1,70 @@
+/**
+ * Created by Shubham on 1/23/2019.
+ */
+({
+        invoke : function(component, event, helper) {
+            component.set('v.SelectedValue','');
+            console.log('invoke>>> : ' + component.get("v.SelectedStatusOrder"));
+            if(!component.get('v.isEditScreen')){
+                var oldOrderList = component.get('v.StatusOrder');
+                var res;
+                if(oldOrderList){
+                   res = oldOrderList.split(";");   
+                }
+                var valueForList = [];
+                for (var i = 0; i < res.length; i++) {
+                    valueForList.push({label:res[i],value:res[i],});
+                }
+                component.set("v.options",valueForList);
+            }else{
+                var res;
+                var oldOrderList = component.get('v.StatusOrder');
+                var selectedOrderList = component.get('v.SelectedStatusOrder');
+                if(oldOrderList){
+                	res = oldOrderList.split(";");
+                }
+                var selectedRes = selectedOrderList.split(";");
+                var flag =0;
+                var items = [];
+                for (var i = 0; i < res.length; i++) {
+                    var item = {
+                           "label": res[i],
+                           "value": res[i],
+                        };
+                        items.push(item);
+                }
+                console.log('Items: ' + items);
+                console.log('selectedRes: ' + selectedRes);
+                component.set("v.options", items);
+                component.set("v.values", selectedRes);
+                console.log('Chnage : ');
+                var selectedOptionValue = component.get("v.values");
+                var newOrder= "";
+                for(var i=0; i< selectedOptionValue.length; i++){
+                    if(newOrder.length <=0){
+                        newOrder = selectedOptionValue[i];
+                    }else{
+                        newOrder = newOrder + ";" + selectedOptionValue[i];
+                    }
+                }
+                component.set('v.SelectedValue',newOrder);
+            }
+         },
+
+         handleChange : function(component, event, helper){
+                       console.log('Chnage : ');
+                       var selectedOptionValue = event.getParam("value");
+                       var newOrder= "";
+                       for(var i=0; i< selectedOptionValue.length; i++){
+                           if(newOrder.length <=0){
+                              newOrder = selectedOptionValue[i];
+                           }else{
+                                newOrder = newOrder + ";" + selectedOptionValue[i];
+                           }
+
+                       }
+                       component.set('v.SelectedValue',newOrder);
+
+
+                   }
+})
