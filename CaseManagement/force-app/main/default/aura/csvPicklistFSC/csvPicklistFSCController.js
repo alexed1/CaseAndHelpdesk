ca450451eb7f29053eb0171a161ecd204b65d4aa
Alex Edelstein <aedelstein@salesforce.comm>
2019-06-09 13:02:51 -0700

    tweaks related to cleanup of how zero predecessors is tracked also fixed width on grey buttons

--- CaseManagement/force-app/main/default/aura/csvPicklistFSC/csvPicklistFSCController.js
+++ CaseManagement/force-app/main/default/aura/csvPicklistFSC/csvPicklistFSCController.js
@@ -2,15 +2,30 @@
     // Invoke will update picklist value for rendered picklist
     invoke : function(component, event, helper){
 		var listType = component.get('v.listType');
-        if(listType == 'simple'){
-            helper.generateFromSimpleList(component.get('v.CSVString').split(","), component);         
-        } else //each list member is complex, which means semicolon separated
-        {
-            var stringList = component.get('v.CSVString').split(',');
-            if(stringList){
-                helper.generateFromCommaSeparatedSublist(stringList, component);     
-            } 
+        var curCSVString = component.get('v.CSVString');
+        console.log("curCSVString is: " + curCSVString);
+        if (typeof curCSVString == 'undefined') {
+            //there are no PlanSteps defined yet, so we better not try splitting this string
+            component.set('v.commaSeparatedFinalList', []);
         }
+        else {
+            if(listType == 'simple'){
+            	helper.generateFromSimpleList(curCSVString.split(","), component);         
+            } 
+            else //each list member is complex, which means semicolon separated
+            {
+                var stringList = curCSVString.split(',');
+                console.log("stringList is: " + stringList);
+                if(stringList){
+                    helper.generateFromCommaSeparatedSublist(stringList, component);     
+                } 
+            }
+        }
+        
             
+    },
+    handleChange : function(component, event, helper){
+        console.log("selectedString just changed and is now: " + component.get('v.selectedString'));
     }
+    
 })
